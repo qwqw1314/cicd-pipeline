349593006ac9bb3a6cab1e3629131da2bd306198
qwqw1314 <qwqw1314@gmail.com>
2022-10-18 18:33:39 +0900

    upperlayer bug fix

--- main.go
+++ main.go
@@ -20,11 +20,13 @@ func main() {
 	for fileScanner.Scan() {
 		line := fileScanner.Text()
 		words := strings.Split(line, " ")
-		splitWord := strings.Split(words[3], ",")
-		var upperLayer string
-		if strings.Contains(splitWord[3], "upperlayer") {
-			upperLayer = splitWord[3]
+		if len(splitWord) >= 3 {}
+				splitWord := strings.Split(words[3], ",")
+			var upperLayer string
+			if strings.Contains(splitWord[3], "upperlayer") {
+				upperLayer = splitWord[3]
+			}
+			fmt.Println(upperLayer)
 		}
-		fmt.Println(upperLayer)
 	}
 }
