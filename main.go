21789347540f15f0dd687d948e56b13db956ef8a
qwqw1314 <qwqw1314@gmail.com>
2022-10-21 15:03:50 +0900

    1111

--- main.go
+++ main.go
@@ -9,28 +9,6 @@ import (
 )
 
 func main() {
-	readFile, err := os.Open("/rootfs/proc/1/mounts")
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-
-	for fileScanner.Scan() {
-		line := fileScanner.Text()
-		words := strings.Split(line, " ")
-		if len(words) >= 4 {
-			splitWord := strings.Split(words[3], ",")
-			if len(splitWord) >= 4 {
-				var upperLayer string
-				if strings.Contains(splitWord[3], "upperdir") {
-					upperLayer = splitWord[3]
-					fmt.Println(upperLayer)
-				}
-			}
-		}
-	}
+	fmt.Println("sleep 24 hours......")
 	time.Sleep(time.Hour * 24)
 }
